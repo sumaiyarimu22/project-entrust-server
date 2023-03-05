b0dfffec1c337c8ac3912f46ccccd9de53e996f7
Sumaiya Rimu <sumaiyarimu22@gmail.com>
2023-03-05 21:45:14 +0600

    3 controllers added

--- routes/projectRoute.js
+++ routes/projectRoute.js
@@ -1,29 +1,30 @@
-const { Router } = require("express");
 const express = require("express");
+const {
+  postProject,
+  getAllProjects,
+  getSingleProject,
+} = require("../controllers/projectController");
 
 //router
 const router = express.Router();
 
 //GET ALL PROJECTS
-router.get("/", (req, res) => {
-  res.json({ message: "GET ALL PROJECTS" });
-});
+router.get("/", getAllProjects);
 
 //GET A SINGLE PROJECT
-router.get("/:id", (req, res) => {
-  res.json({ message: "GET A SINGLE PROJECT" });
-});
+router.get("/:id", getSingleProject);
 
 //POST A NEW PROJECT
-router.post("/", (req, res) => {
-  res.json({ message: "POST A NEW PROJECT" });
-});
+router.post("/", postProject);
+
 //DELETE A PROJECT
 router.delete("/:id", (req, res) => {
   res.json({ message: "DELETE A PROJECT" });
 });
+
 //UPDATE A NEW PROJECT
 router.patch("/:id", (req, res) => {
   res.json({ message: "UPDATE A PROJECT" });
 });
+
 module.exports = router;
